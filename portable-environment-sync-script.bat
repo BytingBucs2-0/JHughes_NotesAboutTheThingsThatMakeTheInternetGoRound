f60034666cb6701ed01f4ea2d813a8ee9fc05a95
Joshua M. Hughes <jhughes7@bucs.fsw.edu>
2017-01-21 11:36:20 -0500

    Add ScreenShots and Update portable environment sync script

--- portable-environment-sync-script.bat
+++ portable-environment-sync-script.bat
@@ -1,1 +1,8 @@
-ROBOCOPY ./ C:\Users\hughe\jhughes7@bucs.fsw.edu\Edison\COP2830\DevelopmentEnv\ /MIR
+REM Set Script Variables - Destination Directory
+set dstDir=c:\Users\hughe\jhughes7@bucs.fsw.edu\Edison\COP2830\DevelopmentEnv\
+
+REM Execute Script
+set srcDir=%CD:~0,3%
+ROBOCOPY %srcDir% %dstDir% /MIR
+
+PAUSE
